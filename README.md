caeeba2e81deda05a8dc717e9fa9e97cc5b5c588
navpreetkaur4 <navu5393@icloud.com>
2024-10-23 15:03:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Intermediate Software Development Automated Teller Project
+This project will be developed over the course of several assignments.  Each 
+assignment will build on the work done in the previous assignment(s).  Ultimately, 
+an entire system will be created to manage bank transactions for clients who 
+have one or more bank accounts.
+
+## Author
+Sukhtab Singh Warya
+
+## Assignment
+Assignment1: Classes, Encapsulation and Unit Test Planning.
+In this assignment you will leverage the knowledge gained in Module 01 to develop classes to support a larger system. This is the first of many assignments in this course, each of which will build on the previous assignment in order to produce a complete system. The focus of this assignment aligns with the focus of the first module - that is, classes, encapsulation and unit test planning.
+
+## Encapsulation
+Encapsulation is about keeping the details of how an object works hidden and only allowing access through specific methods.
+
+1. Private Data: BankAccount keeps its balance private, not directly accessible from outside the class.
+2. Controlled Access: Balance can only be changed through methods like deposit() and withdraw().
+3. Data Protection: Encapsulation ensures data integrity by preventing unauthorized access and modifications.
+4. Secure Methods: The Transaction class also hides its internal details and exposes only necessary methods.
---
+++ tests/test_client.py
@@ -0,0 +1,73 @@
+"""
+Description: Unit tests for the Client class.
+Author: ACE Faculty
+Modified by: {Student Name}
+Date: {Date}
+Usage: To execute all tests in the terminal execute 
+the following command:
+    python -m unittest tests/test_client.py
+"""
+import unittest
+from client.client import Client
+from email_validator import EmailNotValidError
+
+class TestClient(unittest.TestCase):
+
+    def test_valid_client(self):
+        """Test creating a client with valid details."""
+        client = Client(client_number=1010, first_name="Navpreet", last_name="Kaur", email_address="navpreetkaur@gmail.com")
+        self.assertEqual(client.client_number, 1010)
+        self.assertEqual(client.first_name, "Navpreet")
+        self.assertEqual(client.last_name, "kaur")
+        self.assertEqual(client.email_address, "navpreetkaur@gmail.com")
+
+    def test_invalid_client_number(self):
+        """Test creating a client with an invalid client number."""
+        with self.assertRaises(ValueError) as context:
+            Client(client_number="not_integer", first_name="Navpreet", last_name="Kaur", email_address="navpreetkaur@gmail.com")
+        self.assertEqual(str(context.exception), "Client number must be an integer.")
+
+    def test_blank_first_name(self):
+        """Test creating a client with a blank first name."""
+        with self.assertRaises(ValueError) as context:
+            Client(client_number=1010, first_name=" ", last_name="Kaur", email_address="navpreetkaur@gmail.com")
+        self.assertEqual(str(context.exception), "First name cannot be blank.")
+
+    def test_blank_last_name(self):
+        """Test creating a client with a blank last name."""
+        with self.assertRaises(ValueError) as context:
+            Client(client_number=1010, first_name="Navpreet", last_name="Kaur", email_address="navpreetkaur@gmail.com")
+        self.assertEqual(str(context.exception), "Last name cannot be blank.")
+
+    def test_invalid_email(self):
+        """Test creating a client with an invalid email address."""
+        client = Client(client_number=1010, first_name="Navpreet", last_name="Kaur", email_address="navpreetkaur@gmail.com")
+        self.assertEqual(client.email_address, "email@pixell-river.com")
+
+    def test_client_number_property(self):
+        """Test the client_number property."""
+        client = Client(client_number=1010, first_name="Navpreet", last_name="Kaur", email_address="navpreetkaur@gmail.com")
+        self.assertEqual(client.client_number, 1010)
+
+    def test_first_name_property(self):
+        """Test the first_name property."""
+        client = Client(client_number=1010, first_name="Navpreet", last_name="Kaur", email_address="navpreetkaur@gmail.com")
+        self.assertEqual(client.first_name, "Navpreet")
+
+    def test_last_name_property(self):
+        """Test the last_name property."""
+        client = Client(client_number=1010, first_name="Navpreet", last_name="Kaur", email_address="navpreetkaur@gmail.com")
+        self.assertEqual(client.last_name, "Kaur")
+
+    def test_email_address_property(self):
+        """Test the email_address property."""
+        client = Client(client_number=1010, first_name="Navpreet", last_name="Kaur", email_address="navpreetkaur@gmail.com")
+        self.assertEqual(client.email_address, "navpreetkaur@gmail.com")
+
+    def test_str_method(self):
+        """Test the __str__ method."""
+        client = Client(client_number=1010, first_name="Navpreet", last_name="Kaur", email_address="navpreetkaur@gmail.com")
+        self.assertEqual(str(client), "Kaur, Navpreet [1010] - navpreetkaur@gmail.com\n")
+
+if __name__ == '__main__':
+    unittest.main()
